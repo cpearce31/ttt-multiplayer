ffdab40f1ad0c4023031cacc8b75096b0343240b
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-12-21 12:58:23 -0500

    Add AJAX functions
    Just `create` and `update` in `assets/scripts/game/api.js`.

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -3,6 +3,28 @@
 const config = require('../config')
 const store = require('../store')
 
-module.exports = {
+const update = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/games/' + store.game.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
 
+const create = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/games/',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  update,
+  create
 }
