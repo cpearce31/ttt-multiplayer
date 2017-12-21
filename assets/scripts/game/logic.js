f9511eaf5e7894bc635943513a1d875d3f2d5c54
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-12-21 08:47:48 -0500

    Add basic game logic methods
    To check for wins, reset the internal representation of the game, and find out
    who's turn it is.

---
+++ assets/scripts/game/logic.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const store = require('../store')
+
+const initGame = () => {
+  store.game = {
+    cells: ['', '', '', '', '', '', '', '', ''],
+    over: false
+  }
+}
+
+const turn = () => {
+  return store.game.cells.filter(cell => cell === 'x').length % 2 ? 'x' : 'o'
+}
+
+// return true if a is a superset of b
+const _superset = (a, b) => b.every(element => a.indexOf(element) !== -1)
+
+const winner = () => {
+  const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const indices = {
+    'x': [],
+    'o': []
+  }
+
+  store.game.cells.forEach((cell, i) => cell && indices[cell].push(i))
+
+  const xWon = wins.filter(win => _superset(indices.x, win)).length > 0
+  const oWon = wins.filter(win => _superset(indices.o, win)).length > 0
+
+  if (xWon) {
+    return 'x'
+  } else if (oWon) {
+    return 'o'
+  } else {
+    return null
+  }
+}
+
+module.exports = {
+  initGame,
+  turn,
+  winner
+}
