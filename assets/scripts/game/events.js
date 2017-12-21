8acf4f092c86c0fe3bb4567807e00f5cae82f77a
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-12-21 09:29:38 -0500

    Add initial draft of `game/event.js`
    It just has a click handler for cells and a reset game function right now.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -2,9 +2,35 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const store = require('../store')
+const logic = require('./logic')
+
+const onClickCell = function (event) {
+  if (store.game.over) {
+    return
+  }
+
+  const index = event.target.dataset.id
+  const symbol = logic.turn()
+
+  store.game.cells[index] = symbol
+
+  const over = !!logic.winner() || logic.tie()
+
+  api.update(index, symbol, over)
+    .then(ui.onUpdateSuccess)
+    .catch(ui.onUpdateFail)
+}
+
+const onReset = function () {
+  api.create()
+    .then(ui.createGameSuccess)
+    .catch(ui.createGameFail)
+}
 
 const addHandlers = () => {
-
+  $('.cell').on('click', onClickCell)
+  $('#reset').on('click', onReset)
 }
 
 module.exports = {
