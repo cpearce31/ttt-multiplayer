c2339b27be338c5602ea1f63811acc07fc891e8a
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-12-21 12:57:15 -0500

    Remove console.logs
    From `auth` directory only.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,51 +5,45 @@ const store = require('../store')
 const signUpSuccess = function (data) {
   $('#message').text('Signed up successfully')
   $('#message').css('background-color', 'green')
-  console.log(data)
 }
 
-const signUpFailure = function (error) {
+const signUpFailure = function () {
   $('#message').text('Error on sign up')
   $('#message').css('background-color', 'red')
-  console.error(error)
 }
 
 const signInSuccess = function (response) {
   $('#message').text('Signed in successfully')
   $('#message').css('background-color', 'green')
-  console.log('signIn success ran. data is :', response)
+
   store.user = response.user
 }
 
-const signInFailure = function (error) {
+const signInFailure = function () {
   $('#message').text('Error on sign in')
   $('#message').css('background-color', 'red')
-  console.log('signIn failure ran. error is :', error)
 }
 
 const signOutSuccess = function () {
   $('#message').text('Signed out successfully')
   $('#message').css('background-color', 'green')
-  console.log('signOut success ran. and nothing was returned')
+
   store.user = null
 }
 
-const signOutFailure = function (error) {
+const signOutFailure = function () {
   $('#message').text('Error on sign out')
   $('#message').css('background-color', 'red')
-  console.log('signOut failure ran. error is :', error)
 }
 
 const changePasswordSuccess = function () {
   $('#message').text('Changed password successfully')
   $('#message').css('background-color', 'green')
-  console.log('changePassword success ran. and nothing was returned')
 }
 
-const changePasswordFailure = function (error) {
+const changePasswordFailure = function () {
   $('#message').text('Error on change password')
   $('#message').css('background-color', 'red')
-  console.log('changePassword failure ran. error is :', error)
 }
 
 module.exports = {
