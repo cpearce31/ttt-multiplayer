dbc1ff3454c172f55a5aa94c44c9913084d68bf4
caleb-pearce <caleb.pearce@generalassemb.ly>
2017-12-21 13:04:36 -0500

    Attach game related event listeners
    By calling `addHandlers()` in `index.js`.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,9 +13,11 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-const authEvents = require('./auth/events.js')
+const authEvents = require('./auth/events')
+const gameEvents = require('./game/events')
 
 // On document ready
 $(() => {
   authEvents.addHandlers()
+  gameEvents.addHandlers()
 })
